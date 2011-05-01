ffeaf4275ee1456f2af883c6972b3522e6a09577
Steve Clay <steve@mrclay.org>
2011-05-01 03:46:06 -0400

    docs update

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # ScopedRole
-Scoped Role will be a PHP5.3 class library for managing a [contextual role-based capability control system](http://www.mrclay.org/2011/04/01/designing-a-contextual-role-based-capability-control-system/) with a SQL backend. Currently `PDO` and `Zend_Db`-based backends are in development, but you can write your own storage adapter that implements the `ScopedRole\IStorage` interface if you like.
+Scoped Role is a PHP5.3 library for managing a [contextual role-based capability control system](http://www.mrclay.org/2011/04/01/designing-a-contextual-role-based-capability-control-system/) with a SQL backend. Currently `PDO` and `Zend_Db`-based backends are available, but there are interfaces available if you want to write your own.
+
+This project aims to be generic enough to be dropped into any PHP framework. You provide the user IDs, capabilities and roles, and ScopedRole gives you a simple API to manage them.
+
+## Requirements
+PHP 5.3.1
+Any database supported by PDO/Zend_Db
+An autoloader implementation for loading Zend/NotORM class files
 
 ## Status
-None of the code is tested yet. AT ALL.
+All tests are passing, but still alpha code. Name/API changes might be around the corner.
 
 ## API
-The API is still coalescing, but basically you'll inject a `PDO` or an `Zend_Db_Adapter` and an optional table prefix into a storage container, then inject this into a `Core` object. Using that you can spawn a `Core_Editor` object, which contains the front-end API for managing contexts, roles, and capabilities. Or you can create a lighter-weight object to do more common queries with a key-value cache speeding up things.
+Basically you create a "Storage" container wrapping a `PDO` or `Zend_Db_Adapter_Abstract`. From this you can spawn a lightweight `UserContext` value object (e.g. to store in a session) or `Context` object for real-time querying. To improve performance on most requests, the storage implementations have all their modification methods in a separate `Editor` class. By relying on cached `UserContext` objects, most requests only require one small autoloaded class.
 
 ## TODO
-After looking at the edit API (example.php), it's obvious something a bit more ActiveRecord-y would be nicer than passing around ints and pre-checking for the existence of rows.
+More documentation, examples, etc. Implementation into a working system to see if the API needs more work.
--- example.php
+++ example.php
@@ -1,42 +1,29 @@
 <?php
-// example usage of the API (early)
+/*
+ * Example usage of the ScopedRole API
+ *
+ * @todo finish this!
+ */
 
+// setup your autoloading...
 
-use ScopedRole\Core;
-use ScopedRole\Storage_ZendDb;
+// set up sotrage. also available in Zend_Db
+$storage = new ScopedRole\Storage_NotORM(new PDO("..."));
 
-// setup Core
-$db = new Zend_Db_Adapter_Mysqli($config);
-$storage = new Storage_ZendDb($db);
-$core = new Core($storage);
+$contextId = 1; // the scope in which users have role/capability relationships
 
-// most requests use light API for queries
-// e.g. you know context key and userId
-$contextId = $storage->fetchId('contexts', $contextTitle);
-// this UserContext obj pre-fetches roles/capabilities (and in future versions will
-// use a KV cache to speed this up)
-$userContext = new ScopedRole\UserContext($core, $contextId, $userId);
-if ($userContext->hasCapability('can edit forum')) { // a quick in_array check
-    // allow user to edit forum
+// storing UserContext in a session...
+$sessKey = 'scrl_userContext';
+if (isset($_SESSION[$sessKey]) && $_SESSION[$sessKey] instanceof ScopedRole\UserContext) {
+    $uc = $_SESSION[$sessKey]; /* @var $uc ScopedRole\UserContext */
+    if (! $uc->isFresh($ttl)) {
+        $uc = $uc->getRefreshed($storage);
+        $_SESSION[$sessKey] = $uc;
+    }
+} else {
+    $_SESSION[$sessKey] = $storage->fetchUserContext($userId);
 }
 
-// heavier API reserved for when you need to make edits
-// not really happy with this API, but it works
-$editor = $core->getEditor();
-// create some things
-$contextTypeId = $editor->createContextType('course');
-$contextId = $editor->createContext('course 123', $contextTypeId);
-$studentRoleId = $editor->createRole('student');
-$teacherRoleId = $editor->createRole('teacher');
-$viewCapabilityId = $editor->createCapability('can view course');
-$editCapabilityId = $editor->createCapability('can edit course');
-// link capabilities and roles
-$editor->addCapabilityToRole($viewCapabilityId, $studentRoleId);
-$editor->addCapabilityToRole($viewCapabilityId, $teacherRoleId);
-$editor->addCapabilityToRole($editCapabilityId, $teacherRoleId);
-// assignments within a context
-$context = new ScopedRole\Context($core, $contextId);
-$context->grantRole($studentRoleId, $userId);
-// give a user an extra capability outside of roles
+// using it to get roles
 
-$context->grantCapability($editor->createCapability('can eat cake'), $userId);
+// ...
