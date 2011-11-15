5f42f232bde540bb82a17010d1c8f6b43d4c4ff5
Steve Clay <steve@mrclay.org>
2011-11-15 13:15:31 -0500

    More real-world example

--- example.php
+++ example.php
@@ -7,23 +7,30 @@
 
 // setup your autoloading...
 
-// set up sotrage. also available in Zend_Db
+// set up storage. also available in Zend_Db
 $storage = new ScopedRole\Storage_NotORM(new PDO("..."));
 
-$contextId = 1; // the scope in which users have role/capability relationships
-
-// storing UserContext in a session...
-$sessKey = 'scrl_userContext' . $contextId;
-if (isset($_SESSION[$sessKey]) && $_SESSION[$sessKey] instanceof ScopedRole\UserContext) {
-    $uc = $_SESSION[$sessKey]; /* @var $uc ScopedRole\UserContext */
-    if (! $uc->isFresh($ttl)) {
-        $uc = $uc->getRefreshed($storage);
-        $_SESSION[$sessKey] = $uc;
+// Sample function to get a UserContext object and propogate it in $_SESSION
+function userCode_getUserContext(ScopedRole\IStorage $storage, $userId, $contextId) {
+    $sessKey = 'scrl_userContext' . $contextId;
+    if (isset($_SESSION[$sessKey]) && $_SESSION[$sessKey] instanceof ScopedRole\UserContext) {
+        $uc = $_SESSION[$sessKey]; /* @var $uc ScopedRole\UserContext */
+        if (! $uc->isFresh($ttl)) {
+            $uc = $uc->getRefreshed($storage);
+            $_SESSION[$sessKey] = $uc;
+        }
+    } else {
+        $_SESSION[$sessKey] = $storage->fetchUserContext($userId, $contextId);
     }
-} else {
-    $_SESSION[$sessKey] = $storage->fetchUserContext($userId, $contextId);
+    return $_SESSION[$sessKey];
 }
 
-// using it to get roles
+// fetching/querying the UserContext object
+$userId = 123;
+$contextId = 1;
+$userContext = userCode_getUserContext($storage, $userId, $contextId);
+$userContext->hasRole('manager'); // bool
+$userContext->hasCapability('breakThings'); // bool
+
 
 // ...
